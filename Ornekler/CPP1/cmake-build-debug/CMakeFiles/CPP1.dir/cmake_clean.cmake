835d59f6b1ba88fc545e4aa05f59acd5ee588b43
Huseyin Yuce <huseyin@marmara.edu.tr>
2019-11-02 21:37:35 +0300

    Ornekler/CPP6/

---
+++ Ornekler/CPP1/cmake-build-debug/CMakeFiles/CPP1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPP1.dir/main.cpp.o"
+  "CPP1.pdb"
+  "CPP1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPP1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Ornekler/CPP2/cmake-build-debug/CMakeFiles/CPP2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPP2.dir/main.cpp.o"
+  "CPP2.pdb"
+  "CPP2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPP2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
