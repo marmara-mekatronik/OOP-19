34042eaed29040cb47cfafba21e3631a9481086e
yusufcanyavic <yusufcan.yavic4@gmail.com>
2019-11-27 13:08:07 +0300

    Chrono Örnek ekledim

---
+++ Ornekler/Yusufcan_Yavic/DikdörtgenCizmeChronoOrnek/cmake-build-debug/CMakeFiles/untitled21.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled21.dir/main.cpp.obj"
+  "libuntitled21.dll.a"
+  "untitled21.exe"
+  "untitled21.exe.manifest"
+  "untitled21.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled21.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
